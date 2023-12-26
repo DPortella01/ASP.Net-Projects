8de689cbe378ef917707c93f9d335de40a5c3600
DPortella01 <dportella01@mynbcc.ca>
2023-12-26 14:49:26 -0400

    MVCDriver with Authorization -DiegoPortella

---
+++ mvcDriverWithAuth/mvcDriverWithAuth/Models/DriverMakeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace mvcDriverWithAuth.Models
+{
+    public class DriverMakeViewModel
+    {
+        public IEnumerable<Driver> Drivers { get; set; }
+        public IEnumerable<Make> Makes { get; set; }
+
+        public bool ShowModifyLinks { get; set; }
+    }
+}
